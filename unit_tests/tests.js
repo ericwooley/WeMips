092f4866ae80ac2b42a0ea4550a5adc5fd2bcb45
Eric Wooley <5wooley4@gmail.com>
2013-11-20 22:40:53 -0800

    Added more lines to test

--- unit_tests/tests.js
+++ unit_tests/tests.js
@@ -3,7 +3,11 @@ Label:Instruction $s1, $s2, $s3\n\
 Instruction $s1, $s2, $s3\n\
 Instruction $s1, 16\n\
 JAL Lable\n\
-J Lable";
+J Lable\n\
+LW $s1, 16( $sp )\n\
+ADDI $s1, $s2, 16\n\
+ADD $s1, $s2, $s3 #testing it out\n\
+ADD $s1, $s2, $s3 # testing it out";
 console.log("Input:");
 console.log(test_string);
 var me = mips_emulator();
