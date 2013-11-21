5dffb32596d7bb8f7b6e3a50bbb8d716de572304
Eric Wooley <5wooley4@gmail.com>
2013-11-21 13:52:00 -0800

    Now the editor reanalyzes the code if the contents have changed.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     var me = mips_emulator();
+    // The next time we go to step or run, we need to reset the code from the text editor.
+    me.outdated = true; 
     console.log("Editor value: "+$("#editor").val());
     var editor = CodeMirror.fromTextArea(
         document.getElementById("editor"),{
@@ -8,15 +10,27 @@ $(document).ready(function(){
           matchBrackets: true,
       //viewportMargin: 'Infinity'
     });
-    editor.on('blur', function(){
-        alert('Analyzing code');
-        me.setCode($("#editor").val());
+    editor.on('change', function(){
+        
+        me.valid = false;
     });
     $("#step").click(function(){
+        // if this code is no longer valid, reanalyze.
+        if(!me.valid){
+            editor.save();
+            me.setCode($("#editor").val());
+            me.valid = true;
+        } 
       // TODO: hook this up to a mips engine
       alert("Step through function here");
     });
     $("#run").click(function(){
+        // if this code is no longer valid, reanalyze.
+        if(!me.valid){
+            editor.save();
+            me.setCode($("#editor").val());
+            me.valid = true;
+        } 
       // TODO: hook this up to a mips engine
       alert("Run Fucntion here");
     });
