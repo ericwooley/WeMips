8d1623e59bdd93de687e3166043c789f41272234
Ralf Gerlich <kontakt@ralfgerlich.biz>
2023-04-16 10:05:18 +0200

    Add docs for the new parser and simplify a bit

--- js/parser/tokenstream.js
+++ js/parser/tokenstream.js
@@ -1,7 +1,14 @@
+/** A TokenStream represents a stream of tokens
+ * @constructor
+ * @param {Parser.Lexer}  lexer   The lexer to source the tokens from
+ */
 Parser.TokenStream = function(lexer) {
     this.lexer = lexer;
     this.token = undefined;
 
+    /** Take a look at the current token without consuming it
+     * @returns {Parser.Token}   The current token
+     */
     this.peekToken = function() {
         if (this.token == undefined) {
             this.token = this.lexer.next();
@@ -9,16 +16,28 @@ Parser.TokenStream = function(lexer) {
         return this.token;
     }
 
+    /** Check if the next token has a specific type
+     * @param {string}  type   The token type to check for
+     * @returns {boolean} <code>true</code> if and only if the next token has the specified type.
+     */
     this.checkNext = function(type) {
         let token = this.peekToken();
         return (token.type == type);
     }
 
+    /** Get the next token, consuming the current token in the process, if any
+     * @returns {Parser.Token}   The (new) current token
+     */
     this.nextToken = function() {
         this.token = this.lexer.next();
         return this.token;
     }
 
+    /** Check the current token for the given token type, raising an error on mismatch, otherwise fetching the next token
+     * @param {string}  type   The type to check for in the current token
+     * @returns {Parser.Token}   The (new) current token
+     * @throws {Parser.ParseError}  The current token must match the given token type
+     */
     this.consume = function(type) {
         let token = this.peekToken();
         if (type && token.type != type) {
@@ -28,13 +47,21 @@ Parser.TokenStream = function(lexer) {
         return token;
     }
 
+    /** Throw an {@link Parser.ParseError} if there is text remaining.
+     * This is to be used to ensure complete parsing of an input.
+     * @throws {Parser.ParseError} The end of the input must have been reached
+     */
     this.enforceCompletion = function() {
-        if (!this.checkNext(Parser.Tokens.EndOfString)) {
+        if (!this.checkNext(Parser.TokenType.EndOfString)) {
             throw new Parser.ParseError('Trailing text remaining');
         }
     }
 }
 
+/** Create a token stream from an input string
+ * @param {string} input   The string to scan
+ * @returns {Parser.TokenStream}  The token stream providing the tokens from the input string
+ */
 Parser.tokenStreamFromString = function(input) {
     let lexer = new Parser.Lexer(input);
     return new Parser.TokenStream(lexer);
--- js/parser/utils.js
+++ js/parser/utils.js
@@ -1,11 +1,24 @@
 let Parser;
 Parser = Parser || {};
 
+/** Base class for all parser exceptions
+ * @constructor
+ * @param {string} name The kind of exception
+ * @param {string} message A human-readable message explaining the kind of exception
+ */
 Parser.Error = function(name, message) {
     this.name = name;
     this.message = message;
 }
 
+/** Exception for lexer errors
+ * Thrown when the lexer cannot associate part of the input with a lexeme.
+ * @constructor
+ * @param {string} message A human-readable message explaining the kind of exception
+ * @param {string} text    The text of the relevant token or character as processed so far
+ * @param {number} start   The start index of the text within the input string
+ * @param {number} end     The index of the next character within the input string plus one
+ */
 Parser.LexerError = function(message, text, start, end) {
     Parser.Error.call(this, 'LexerError', message);
     this.text = text;
@@ -14,6 +27,12 @@ Parser.LexerError = function(message, text, start, end) {
 }
 Parser.LexerError.prototype = Object.create( Parser.Error.prototype );
 
+/** Exception for parsing errors
+ * Thrown when the parser cannot associate a token with a grammar rule.
+ * @constructor
+ * @param {string} message       A human-readable message explaining the kind of exception
+ * @param {Parser.Token} token   The relevant token that led to the exception
+ */
 Parser.ParseError = function(message, token) {
     Parser.Error.call(this, 'ParseError', message);
     this.token = token;
